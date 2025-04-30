4a7ea8460e891a3d620f017a7c581e6398138e7a
Austin Williams <senortinman@gmail.com>
2025-04-30 01:05:18 -0400

    Add git blame plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,8 +2,8 @@ return {
   { --Main git client in neovim
     'NeogitOrg/neogit',
     dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-lua/plenary.nvim',         -- required
+      'sindrets/diffview.nvim',        -- optional - Diff integration
       'nvim-telescope/telescope.nvim', -- optional
     },
     config = true,
@@ -19,6 +19,24 @@ return {
         changedelete = { text = '~' },
       },
     },
+  },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin will only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+
   },
   --GOTTA be able to see that diff
   { 'sindrets/diffview.nvim' },
